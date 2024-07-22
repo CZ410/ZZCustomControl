a5ed74ab326023ce0e1d316cbcceec36a47cf2bc
czz_8032 <czz_8023@qq.com>
2024-07-22 14:02:38 +0800

    zztabcontrol

--- Example/ZZCustomControl/ViewController.swift
+++ Example/ZZCustomControl/ViewController.swift
@@ -16,6 +16,62 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         let line = ZZScrollView.Item(line: .red)
+
+        let tabControl = ZZTabControl().zz_backgroundColor(.orange)
+        tabControl.titles = ["九分裤拉倒", "九分裤拉倒", "九分拉倒", "九裤拉倒", "分裤拉倒", "分裤", "倒", "裤拉倒", "九分裤拉倒拉倒拉倒"]
+//        tabControl.titles = ["九分裤拉倒", "九分裤拉倒", ]
+        self.view.addSubview(tabControl)
+        tabControl.zz_frame(CGRect(x: 10, y: 200, width: 300, height: 50))
+
+        tabControl.titleFont = .systemFont(ofSize: 14, weight: .medium)
+        tabControl.inset = .zz_all(5)
+//        tabControl.seletedIndex = 2
+//        tabControl.setSelectedIndex(2, animate: true)
+        tabControl.scrollIndicatorAlignment = .bottom
+        tabControl.scrollIndicatorFixedHeight = 4
+        tabControl.scrollIndicator.zz_cornerRadius(2)
+        tabControl.selectedScale = 1.5
+        tabControl.isBackScrollIndicator = true
+        tabControl.scrollIndicatorMoreSize = .zz_only(width: 10)
+        tabControl.contentAlignment = .right
+//        tabControl.animateTime = 1
+        tabControl.scrollIndicatorOffset = .zz_only(y: 5)
+//        tabControl.itemMoreSize = .zz_all(20)
+//        tabControl.fixedSize = CGSize(width: 100, height: 5)
+//        tabControl.itemOffset = .zz_all(10)
+
+        
+
+
+        let label = UILabel(text: "jfdlajfldsafds")
+            .zz_font(.systemFont(ofSize: 20, weight: .bold))
+            .zz_textColor(.black)
+
+        label.zz_addTap { sender in
+            UIView.transition(with: label,
+                              duration: 0.25,
+                              options: .transitionCrossDissolve,
+                              animations: { [weak self] in
+                guard let `self` = self else { return }
+                label.zz_textColor(label.textColor == .red ? .black : .red)
+                label.transform = label.transform == .identity ? CGAffineTransform(scaleX: 1.5, y: 1.5) : .identity
+
+                tabControl.zz_width += 10
+            })
+        }
+
+        view.addSubview(label)
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+        ])
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            label.zz_textColor(.red)
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Sources/Classes/ZZTabControl/ZZTabControl.swift
@@ -0,0 +1,343 @@
+//
+//  ZZTabControl.swift
+//  Pods
+//
+//  Created by 陈钟 on 2023/10/7.
+//
+
+import Foundation
+import ZZBase
+
+public extension ZZTabControl{
+    enum ContentAlignment {
+        case left, center, right
+    }
+
+    enum ScrollIndicatorAlignment {
+        case top, center, bottom
+    }
+
+}
+
+open class ZZTabControl: UIView{
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        initUI()
+    }
+
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initUI()
+    }
+
+    open func initUI(){
+        self.addSubview(contentView)
+    }
+
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = CGRect(origin: .zero, size: zz_size)
+        refreshTitlesFrame()
+        refreshSelectedIndex(animate: false)
+    }
+
+    private var titleViewsArr = [(view: UIView, normalSize: CGSize, seletedSize: CGSize)]()
+
+    /// 选中的位置
+    open var seletedIndex: Int = 0
+
+    open func setSelectedIndex(_ index: Int, animate: Bool){
+        refreshSelectedLastIndex(animate: animate)
+        seletedIndex = index
+        refreshSelectedIndex(animate: animate)
+    }
+
+    /// 选中是指示器固定到ZZTabControl中间位置
+    open var isScrollCenter: Bool = true
+    
+    /// 标题
+    open var titles = [String](){
+        didSet{
+            refreshTitles()
+        }
+    }
+
+    open var customViews: [UIView] = []{
+        didSet{
+
+        }
+    }
+
+    open var contentAlignment: ContentAlignment = .center{
+        didSet{
+            refreshTitlesFrame()
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var scrollIndicatorAlignment: ScrollIndicatorAlignment = .center{
+        didSet{
+            refreshTitlesFrame()
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var normalTitleColor: UIColor = .black
+    open var selectedTitleColor: UIColor = .systemBlue
+
+    open var titleFont: UIFont = .systemFont(ofSize: 14, weight: .regular){
+        didSet{
+            refreshTitles()
+        }
+    }
+
+    open var selectedScale: CGFloat = 1.5{
+        didSet{
+            refreshTitles()
+        }
+    }
+
+    /// 动画时间
+    open var animateTime: TimeInterval = 0.25
+
+    open var inset: UIEdgeInsets = .zero{
+        didSet{
+            refreshTitlesFrame()
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var itemOffset: CGPoint = .zero {
+        didSet{
+            refreshTitlesFrame()
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var itemFixedSize: CGSize = .zero{
+        didSet{
+            refreshTitlesFrame()
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var itemMoreSize: CGSize = .zero{
+        didSet{
+            refreshTitles()
+        }
+    }
+
+    open var itemSpacing: CGFloat = 10{
+        didSet{
+            refreshTitlesFrame()
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var scrollIndicatorFixedWidth: CGFloat = 0{
+        didSet{
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var scrollIndicatorFixedHeight: CGFloat = 0{
+        didSet{
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var scrollIndicatorMoreSize: CGSize = .zero{
+        didSet{
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var scrollIndicatorOffset: CGPoint = .zero{
+        didSet{
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    open var isBackScrollIndicator: Bool = false{
+        didSet{
+            refreshSelectedIndex(animate: false)
+        }
+    }
+
+    private func refreshTitles(){
+        titleViewsArr.forEach({ $0.view.superview != nil ? $0.view.removeFromSuperview() : nil })
+        titleViewsArr.removeAll()
+        for i in 0 ..< titles.count {
+            let title = titles[i]
+            let label = UILabel().zz_textAlignment(.center).zz_backgroundColor(.red)
+            label.text = title
+            label.font = titleFont
+
+            label.zz_addTap { [weak self] sender in
+                guard let `self` = self else { return }
+                if self.seletedIndex == i { return }
+                self.setSelectedIndex(i, animate: true)
+            }
+
+            let textSize = title.zz_textSize(font: titleFont, maxSize: CGSize(width: .zz_max, height: zz_height))
+            let selectedSize = CGSize(width: (textSize.width * selectedScale) , height: textSize.height * selectedScale)
+            titleViewsArr.append((label, textSize, selectedSize))
+            contentView.addSubview(label)
+        }
+        refreshTitlesFrame()
+        refreshSelectedIndex(animate: false)
+    }
+
+    private func refreshTitlesFrame(){
+        var x = inset.left
+        for i in 0 ..< titleViewsArr.count {
+            let item = titleViewsArr[i]
+            var size = i == seletedIndex ? item.seletedSize : item.normalSize
+            size.height = zz_height - inset.top - inset.bottom
+            size = itemFixedSize == .zero ? size : itemFixedSize
+            size += itemMoreSize
+
+            x += itemOffset.x
+            let y = inset.top + ((zz_height - inset.top - inset.bottom - size.height) * 0.5) + itemOffset.y
+
+            item.view.frame = CGRect(origin: CGPoint(x: x, y: y), size: size)
+            x = (x + size.width + itemSpacing)
+        }
+        contentView.contentSize = .zz_only(width: x + inset.right - itemSpacing)
+        if contentView.contentSize.width < contentView.zz_width{
+            switch self.contentAlignment {
+                case .left:
+                    contentView.contentInset = .zero
+                case .center:
+                    let inset: CGFloat = (frame.width - contentView.contentSize.width) / 2.0
+                    contentView.contentInset = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: 0)
+                case .right:
+                    let inset: CGFloat = (frame.width - contentView.contentSize.width)
+                    contentView.contentInset = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: 0)
+            }
+        }else{
+            contentView.contentInset = UIEdgeInsets.zero
+        }
+    }
+
+
+    /// 恢复之前选中Item的样式
+    private func refreshSelectedLastIndex(animate: Bool){
+        let item = titleViewsArr[seletedIndex]
+        let selLabel = item.view as? UILabel
+        selLabel?.animateTextColor(to: normalTitleColor, animateTime: animateTime)
+        if animate{
+            UIView.animate(withDuration: animateTime)  {
+                item.view.transform = .identity
+            }
+        }else {
+            item.view.transform = .identity
+        }
+    }
+
+
+    /// 跟新选中Item的样式
+    private func refreshSelectedIndex(animate: Bool){
+        let item = titleViewsArr[seletedIndex]
+        let selLabel = item.view as? UILabel
+        selLabel?.animateTextColor(to: selectedTitleColor, animateTime: animateTime)
+//        item.view.transform = .identity
+
+        if isBackScrollIndicator{
+            contentView.sendSubviewToBack(scrollIndicator)
+        } else {
+            contentView.bringSubviewToFront(scrollIndicator)
+        }
+
+        func refresh(){
+            item.view.transform = CGAffineTransform(scaleX: self.selectedScale, y: self.selectedScale)
+            self.refreshTitlesFrame()
+            let indicatorWidth = self.scrollIndicatorFixedWidth == 0 ? item.view.zz_size.width : self.scrollIndicatorFixedWidth
+            let indicatorHeight = self.scrollIndicatorFixedHeight == 0 ? item.view.zz_size.height : self.scrollIndicatorFixedHeight
+            let size = CGSize(width: indicatorWidth, height: indicatorHeight) + self.scrollIndicatorMoreSize
+            self.scrollIndicator.zz_size(size)
+            switch self.scrollIndicatorAlignment {
+                case .top:
+                    self.scrollIndicator.zz_center(CGPoint(x: item.view.zz_centerX + scrollIndicatorOffset.x, 
+                                                           y: size.height * 0.5 + inset.top + scrollIndicatorOffset.y))
+                case .center:
+                    self.scrollIndicator.zz_center(CGPoint(x: item.view.zz_centerX + scrollIndicatorOffset.x,
+                                                           y: self.zz_height * 0.5 + scrollIndicatorOffset.y))
+                case .bottom:
+                    self.scrollIndicator.zz_center(CGPoint(x: item.view.zz_centerX + scrollIndicatorOffset.x,
+                                                           y: self.zz_height - (size.height * 0.5 + inset.bottom) + scrollIndicatorOffset.y))
+            }
+
+            if isScrollCenter{
+                if self.contentView.contentInset.left != 0 { return }
+                if self.contentView.contentSize.width < self.contentView.frame.width { return }
+                let selectedItemFrame = item.view.frame
+                let wid = zz_width / 2.0
+                let selectItemCenterX = selectedItemFrame.origin.x + selectedItemFrame.width / 2.0
+                if selectItemCenterX > wid && selectItemCenterX < self.contentView.contentSize.width - wid {
+                    self.contentView.contentOffset = CGPoint(x: selectItemCenterX - wid, y: 0)
+                }else if (selectItemCenterX < wid){
+                    self.contentView.contentOffset = .zero
+                }else if (selectItemCenterX > self.contentView.contentSize.width - wid){
+                    self.contentView.contentOffset = CGPoint(x: self.contentView.contentSize.width - self.frame.width, y: 0)
+                }
+            }
+        }
+        if animate{
+            UIView.animate(withDuration: animateTime) {
+                refresh()
+            }
+        }else {
+            refresh()
+        }
+    }
+
+    open lazy var contentView: UIScrollView = {
+        let view = UIScrollView()
+        view.delegate = self
+        view.backgroundColor = .clear
+        view.showsHorizontalScrollIndicator = false
+        view.showsVerticalScrollIndicator = false
+        return view
+    }()
+
+    open lazy var scrollIndicator: UIImageView = {
+        let view = UIImageView()
+            .zz_backgroundColor(.systemBlue)
+        contentView.addSubview(view)
+        return view
+    }()
+}
+
+extension ZZTabControl: UIScrollViewDelegate{
+//    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        ZZLog(scrollView.contentOffset)
+//    }
+}
+
+
+fileprivate extension UILabel{
+    @discardableResult
+    func animateTextColor(to color: UIColor, animateTime: TimeInterval) -> Self {
+        UIView.transition(with: self,
+                          duration: animateTime,
+                          options: .transitionCrossDissolve,
+                          animations: { [weak self] in
+            guard let `self` = self else { return }
+            self.textColor = color
+        })
+        return self
+    }
+
+    @discardableResult
+    func animateFont(to font: UIFont, animateTime: TimeInterval) -> Self{
+        UIView.transition(with: self,
+                          duration: animateTime,
+                          options: .transitionCrossDissolve,
+                          animations: { [weak self] in
+            guard let `self` = self else { return }
+            self.font = font
+        })
+        return self
+    }
+}
